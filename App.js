75b4377d3bfdc6620ff6fc20f1bbd4789e30e467
Lahiru Amarathunga <lahiruamarathunge94@gmail.com>
2020-03-01 12:42:17 +0530

    updates

--- App.js
+++ App.js
@@ -1,176 +1,26 @@
 import * as React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  TouchableOpacity,
-  Button,
-  ScrollView,
-} from 'react-native';
+import {Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
-const CustomHeader = ({title, isHome, navigation}) => {
-  return (
-    <View style={styles.header}>
-      <View style={styles.col1}>
-        {isHome ? (
-          <TouchableOpacity onPress={() => navigation.openDrawer()}>
-            <Image
-              style={styles.image}
-              source={require('./src/images/menu.png')}
-              resizeMode="contain"
-            />
-          </TouchableOpacity>
-        ) : (
-          <TouchableOpacity
-            style={styles.back}
-            onPress={() => navigation.goBack()}>
-            <Image
-              style={styles.imageBack}
-              source={require('./src/images/left.png')}
-              resizeMode="contain"
-            />
-            <Text style={styles.backText}>Back</Text>
-          </TouchableOpacity>
-        )}
-      </View>
+import {CustomHeader} from './src/common/CustomHeader';
+import {CustomDrawerContent} from './src/common/CustomDrawer';
 
-      <View style={styles.col2}>
-        <Text> {title}</Text>
-      </View>
-      <View style={styles.col3} />
-    </View>
-  );
-};
+import {
+  HomeScreen,
+  HomeDetailScreen,
+  SettingsScreen,
+  SettingsDetailScreen,
+} from './src/tab';
 
-function HomeScreen({navigation}) {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <CustomHeader title="Home" isHome={true} navigation={navigation} />
-      <View style={styles.home}>
-        <Text>Home!</Text>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate('HomeDetail')}>
-          <Text>Go Home Detail</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
-}
-
-function HomeDetailScreen({navigation}) {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <CustomHeader title="Home Detail" navigation={navigation} />
-      <View style={styles.home}>
-        <Text>Home Detail!</Text>
-      </View>
-    </SafeAreaView>
-  );
-}
-
-function SettingsScreen({navigation}) {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <CustomHeader title="Settings" isHome={true} navigation={navigation} />
-      <View style={styles.setting}>
-        <Text>Settings!</Text>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate('SettingDetail')}>
-          <Text>Go Setting Detail</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
-}
-
-function SettingsDetailScreen({navigation}) {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <CustomHeader title="Settings Detail" navigation={navigation} />
-      <View style={styles.setting}>
-        <Text>Settings Detail!</Text>
-      </View>
-    </SafeAreaView>
-  );
-}
+import {NotificationsScreen} from './src/drawer';
+import {LoginScreen, RegisterScreen} from './src/auth';
+import {IMAGE} from './src/constants/images';
 
 const StackHome = createStackNavigator();
 const StackSetting = createStackNavigator();
-
-function NotificationsScreen({navigation}) {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <CustomHeader title="Notifications" navigation={navigation} />
-      <View style={styles.setting}>
-        <Text>Notifications Screen!</Text>
-      </View>
-    </SafeAreaView>
-  );
-}
-
-function RegisterScreen({navigation}) {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <CustomHeader title="Register" navigation={navigation} />
-      <View style={styles.setting}>
-        <Text>Register Screen!</Text>
-      </View>
-    </SafeAreaView>
-  );
-}
-
-function LoginScreen({navigation}) {
-  return (
-    <SafeAreaView style={{flex: 1}}>
-      <View style={styles.setting}>
-        <Text>Login Screen!</Text>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate('HomeApp')}>
-          <Text>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate('Register')}>
-          <Text>Register</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
-  );
-}
-
-function CustomDrawerContent(props) {
-  return (
-    <SafeAreaView style={styles.drawerWrapper}>
-      <View style={styles.profile}>
-        <Image
-          source={require('./src/images/profile.png')}
-          style={styles.profileImage}
-        />
-      </View>
-      <ScrollView>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => props.navigation.navigate('MenuTab')}>
-          <Text>Menu Tab</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => props.navigation.navigate('Notifications')}>
-          <Text>Notifications</Text>
-        </TouchableOpacity>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
 
 const Tab = createBottomTabNavigator();
 
@@ -220,13 +70,9 @@ function TabNavigator() {
           let iconName;
 
           if (route.name === 'Home') {
-            iconName = focused
-              ? require('./src/images/home-color.png')
-              : require('./src/images/home.png');
+            iconName = focused ? IMAGE.ICON_HOME_FOCUS : IMAGE.ICON_HOME;
           } else if (route.name === 'Settings') {
-            iconName = focused
-              ? require('./src/images/setting-color.png')
-              : require('./src/images/setting.png');
+            iconName = focused ? IMAGE.ICON_SETTING_FOCUS : IMAGE.ICON_SETTING;
           }
 
           // You can return any component that you like here!
@@ -287,71 +133,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  header: {
-    flexDirection: 'row',
-    height: 50,
-  },
-  image: {
-    height: 25,
-    width: 25,
-    marginLeft: 10,
-  },
-  imageBack: {
-    height: 30,
-    width: 30,
-    marginLeft: 10,
-  },
-  col1: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  col2: {
-    flex: 1.5,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  col3: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  home: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  setting: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    marginTop: 20,
-    backgroundColor: 'white',
-    padding: 10,
-  },
-  back: {
-    flexDirection: 'row',
-    //justifyContent: 'center',
-  },
-  backText: {
-    textAlign: 'center',
-    alignItems: 'center',
-    padding: 5,
-  },
-  drawerWrapper: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-  },
-  profile: {
-    height: 150,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  profileImage: {
-    height: 100,
-    width: 100,
-    borderRadius: 60,
-  },
-});
---
+++ src/auth/LoginScreen.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class LoginScreen extends Component {
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <View style={styles.setting}>
+          <Text>Login Screen!</Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.props.navigation.navigate('HomeApp')}>
+            <Text>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.props.navigation.navigate('Register')}>
+            <Text>Register</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  setting: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: 'white',
+    padding: 10,
+  },
+});
